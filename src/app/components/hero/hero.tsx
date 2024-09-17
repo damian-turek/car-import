f43bb50333d689276e34b41a0121de5a060d8154
Damian Turek <damianturek@icloud.com>
2024-09-17 19:32:36 +0200

    Skeleton of component

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const Hero: React.FC = () => (
     <>
-        Hero
+        <div>
+            <h2>Znajdź ze mną wymarzone auto</h2>
+            <p>Zajmuję się sprowadzaniem samochodów z Niemiec od 15 lat. Zaufało mi ponad 120 klientów.</p>
+            <Link href="#">Skontaktuj się ze mną</Link>
+        </div>
+        <Image
+            src=""
+            alt="Hero"
+            fill={true}
+        />
     </>
 )
