aa90302f104f9dae6e023de94f3ced37d0c846a0
Damian Turek <damianturek@icloud.com>
2024-09-17 19:47:13 +0200

    Styling component

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import HeroImage from "../../../images/hero.jpg"
+
+import styles from "./hero.module.scss"
+
 export const Hero: React.FC = () => (
-    <>
-        <div>
+    <div className={styles.hero}>
+        <div className={styles.motto}>
             <h2>Znajdź ze mną wymarzone auto</h2>
             <p>Zajmuję się sprowadzaniem samochodów z Niemiec od 15 lat. Zaufało mi ponad 120 klientów.</p>
-            <Link href="#">Skontaktuj się ze mną</Link>
+            <Link href="#">
+                <div className={styles.button}>Skontaktuj się ze mną</div>
+            </Link>
         </div>
         <Image
-            src=""
+            className={styles.image}
+            src={HeroImage}
             alt="Hero"
             fill={true}
         />
-    </>
+    </div>
 )
