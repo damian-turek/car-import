95215d775d8ef7150ce3b1d5b32a3110a8fc5ddc
Damian Turek <damianturek@icloud.com>
2024-11-09 17:30:46 +0100

    Gallery

---
+++ src/app/components/gallery/gallery.tsx
@@ -0,0 +1,34 @@
+import { client } from '@/app/contentful/contentful'
+import { ContentfulGallery } from '@/app/contentful/types/gallery'
+import React from 'react'
+
+import styles from './gallery.module.scss'
+import Image from 'next/image'
+
+export const Gallery = async () => {
+    const entries = await client.withoutUnresolvableLinks.getEntries<ContentfulGallery>({
+        content_type: 'carGalery',
+        order: ['fields.id']
+    })
+
+    const carGallery = entries.items
+
+    return (
+        <div>
+            <h3>Galeria</h3>
+            <div className={styles.gallery}>
+                {carGallery.map((car) => (
+                    <div className={styles.photo} key={car.fields.id}>
+                        <Image
+                            src={`https:${car.fields.carImage?.fields.file?.url}`}
+                            width={10}
+                            height={7}
+                            alt={car.fields.carModel}
+                        />
+                        <h4>{car.fields.carModel}</h4>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
