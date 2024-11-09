95225dbae8c6f1b12813dba76c05fe8acb3ebfa0
Damian Turek <damianturek@icloud.com>
2024-11-09 16:28:11 +0100

    Fixes

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,27 +1,19 @@
 import styles from "./header.module.scss"
 import Link from 'next/link'
 import React from 'react'
-import Image from 'next/image'
-
-import Logo from "../../../images/icons/logo.png"
 
 export const Header: React.FC = () => {
     return (
         <header className={styles.header}>
             <div className={styles.page}>
                 <Link href="/" className={styles.logo}>
-                    <Image
-                        src={Logo}
-                        alt="logo"
-                        width={75}
-                    />
                     Bartek-Auta.pl
                 </Link>
                 <nav>
-                    <Link href="/AutoZNiemiec">Dlaczego auto z Niemiec?</Link>
                     <Link href="/DlaczegoZeMna">Dlaczego ze mną?</Link>
+                    <Link href="/AutoZNiemiec">Dlaczego auto z Niemiec?</Link>
                     <Link href="/CoSprawdzam">Co sprawdzam?</Link>
-                    <Link href="#">Kontakt</Link>
+                    <Link href="/Kontakt">Kontakt</Link>
                 </nav>
             </div>
         </header>
