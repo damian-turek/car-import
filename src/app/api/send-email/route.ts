7c3fd2c2ee90159ae23dffd017d84273994674d6
damianturek <damianturek@icloud.com>
2024-12-03 00:38:46 +0100

    fix2

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -6,6 +6,7 @@ export async function POST(req: NextRequest) {
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
+        logger: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -14,34 +15,12 @@ export async function POST(req: NextRequest) {
         port: 465,
     });
 
-    await new Promise((resolve, reject) => {
-        transporter.verify(function (error, success) {
-            if(error) {
-                console.log(error)
-                reject(error)
-            } else {
-                resolve(success)
-            }
-        })
-    })
-
     const mailOptions = {
         from: email,
         to: process.env.RECIPMENT_EMAIL,
         subject: topic,
         text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
     }
-
-    await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) {
-                console.log(err)
-                reject(err)
-            } else {
-                resolve(info)
-            }
-        })
-    })
 
     try {
         await transporter.sendMail(mailOptions);
