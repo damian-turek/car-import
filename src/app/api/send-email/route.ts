9d53e2d758827ce725ed83ac7ce2bc0fef064408
damianturek <damianturek@icloud.com>
2024-12-02 23:46:03 +0100

    nodemailer-fix

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -6,12 +6,9 @@ export async function POST( req: NextRequest ) {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            pass: process.env.EMAIL_PASS,
         },
     })
 
