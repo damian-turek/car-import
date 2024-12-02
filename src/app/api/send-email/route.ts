3881bd9c8869a0ca7d3304f596a23fc415c179ec
damianturek <damianturek@icloud.com>
2024-12-03 00:01:06 +0100

    fix

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,29 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 
-export async function POST( req: NextRequest ) {
+export async function POST(req: NextRequest) {
     const { name, email, topic, message } = await req.json();
 
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
-    })
+        secure: true,
+        port: 465,
+    });
 
     const mailOptions = {
         from: email,
         to: process.env.RECIPMENT_EMAIL,
         subject: topic,
-        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
-    }
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+    };
 
     try {
-        await transporter.sendMail(mailOptions)
-        return NextResponse.json({ message: 'Wiadomość wysłana!' }/*, { status: 200 }*/)
+        await transporter.sendMail(mailOptions);
+        return NextResponse.json({ message: 'Wiadomość wysłana!' });
     } catch (error) {
-        console.error('Error sending email: ', error)
-        return NextResponse.json({ message: 'Błąd wysyłania wiadomości' }/*, { status: 500 }*/);
+        console.error('Error sending email: ', error);
+        return NextResponse.json({ message: 'Błąd wysyłania wiadomości' });
     }
 }
