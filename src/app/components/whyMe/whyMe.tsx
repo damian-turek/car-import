4a7bfc674dd742dbd6c05302bc263f50c08eaf91
damianturek <damianturek@icloud.com>
2024-12-02 17:45:00 +0100

    fixing css and add deply file

--- src/app/components/whyMe/whyMe.tsx
+++ src/app/components/whyMe/whyMe.tsx
@@ -12,7 +12,7 @@ export const WhyMe: React.FC = () => (
             <h3 className={styles.subtitle}>Dlaczego ze mną?</h3>
                 <ul className={styles.list}>
                     <li><span>&#10004;</span> pasja od dzieciństwa</li>
-                    <li><span>&#10004;</span> 15 lat doświadczenia w handlu samochodami</li>
+                    <li><span>&#10004;</span> 20 lat doświadczenia w handlu samochodami</li>
                     <li><span>&#10004;</span> znajomość realiów rynków niemieckojęzycznych jak i perfekcyjna znajomość języka niemieckiego</li>
                     <li><span>&#10004;</span> cała transakcja jest obsługiwana wyłącznie przeze mnie (każde auto jest osobiście przeze mnie weryfikowane i sprawdzane)</li>
                     <li><span>&#10004;</span> informowanie klientów na każdym etapie</li>
@@ -24,6 +24,6 @@ export const WhyMe: React.FC = () => (
                 alt="Bartek"
             />
         </div>
-        <p className={styles.description}>Motoryzacja od zawsze była moją pasją, dlatego postanowiłem zająć się nią zawodowo. Wnikliwa obserwacja rynku pozwoliła mi dojść do wniosku, że niemieckie auta używane wciąż cieszą się u nas zasłużoną popularnością. Wychodząc naprzeciw potrzebom osób, które chcą kupić samochód od naszych zachodnich sąsiadów, oferuję kompleksową usługę sprowadzenia wybranego pojazdu na preferencyjnych warunkach. Używane auta z Niemiec to modele renomowanych marek, utrzymane w naprawdę dobrym stanie technicznym. Całym przebiegiem transakcji zajmuję się osobiście, sprawdzając ogólne informacje o pojeździe, jego karoserię, jakość wyposażenia, a także stan silnika, szyb, oświetlenia, kół i ogumienia. Nadzoruję przeprowadzanie diagnostyki komputerowej i kontroli podwozia. Samochody wyróżniają się doskonałymi parametrami, co dodatkowo weryfikuję, odbywając jazdę próbną. Doskonale orientuję się w realiach rynków niemieckojęzycznych. Zaufało mi wielu klientów, którzy z moją pomocą kupili wymarzone auta używane z Niemiec.</p>
+        <p className={styles.description}>Motoryzacja od zawsze była moją pasją, dlatego postanowiłem zająć się nią zawodowo. Wnikliwa obserwacja rynku pozwoliła mi dojść do wniosku, że niemieckie auta używane wciąż cieszą się u nas zasłużoną popularnością. Wychodząc naprzeciw potrzebom osób, które chcą kupić samochód od naszych zachodnich sąsiadów, oferuję kompleksową usługę sprowadzenia wybranego pojazdu na preferencyjnych warunkach. Używane auta z Niemiec to modele renomowanych marek, utrzymane w naprawdę dobrym stanie technicznym. Całym przebiegiem transakcji zajmuję się osobiście, sprawdzając ogólne informacje o pojeździe, jego karoserię, jakość wyposażenia, a także stan silnika, szyb, oświetlenia, kół i ogumienia. Nadzoruję przeprowadzanie kontroli podwozia. Samochody wyróżniają się doskonałymi parametrami, co dodatkowo weryfikuję, odbywając jazdę próbną. Doskonale orientuję się w realiach rynków niemieckojęzycznych. Zaufało mi wielu klientów, którzy z moją pomocą kupili wymarzone auta używane z Niemiec.</p>
     </div>
 )
